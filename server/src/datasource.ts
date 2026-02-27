19eb5dae588b84fddd972dea3b41aaa37133457a
Alexis Godard <alexis.godard56@gmail.com>
2026-02-27 11:47:39 +0100

    Ajout docker compose

--- server/src/datasource.ts
+++ server/src/datasource.ts
@@ -1,9 +1,13 @@
 import { DataSource } from "typeorm";
 
 const datasource = new DataSource({
-  type: "better-sqlite3",
-  database: "./db.sqlite",
-  entities: ["./src/entities/**/*.{js,ts}"],
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: "postgres",
+  password: "supersecretpassword",
+  database: "postgres",
+  entities: [__dirname+"/entities/**/*.{js,ts}"],
   logging: true,
   synchronize: true,
 });
